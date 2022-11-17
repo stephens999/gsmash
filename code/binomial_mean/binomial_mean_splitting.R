5124e6ed2f377a2609e53607b800c144c35846a7
DongyueXie <dongyxie@gmail.com>
2022-11-17 17:33:05 -0600

    add binomial

---
+++ code/binomial_mean/binomial_mean_splitting.R
@@ -0,0 +1,264 @@
+
+## examples
+
+
+
+
+library(vebpm)
+library(fastGHQuad)
+library(ebnm)
+library(Rfast)
+binomial_mean_splitting = function(x,nb=1,
+                                   tol=1e-5,
+                                   maxiter=1e3,
+                                   ebnm_params=NULL,
+                                   optim_method ='L-BFGS-B',
+                                   n_gh = 20,
+                                   printevery = 100,
+                                   b_pm_init = NULL,
+                                   sigma2_init = NULL,
+                                   convergence_criteria = 'obj'){
+  n = length(x)
+  if(length(nb)==1){
+    nb = rep(nb,n)
+  }
+  obj = rep(0,maxiter+1)
+  obj[1] = -Inf
+  if(is.null(ebnm_params)){
+    ebnm_params = ebnm_params_default()
+  }else{
+    temp = ebnm_params_default()
+    for(i in 1:length(ebnm_params)){
+      temp[[names(ebnm_params)[i]]] = ebnm_params[[i]]
+    }
+    ebnm_params = temp
+  }
+
+  # const in objective function
+  const = sum(lfactorial(nb)-lfactorial(x)-lfactorial(nb-x))
+
+  gh_points = gaussHermiteData(n_gh)
+
+  if(is.null(b_pm_init)){
+    b_pm = rep(0,n)
+  }else{
+    b_pm = b_pm_init
+  }
+
+  #b_pv = rep(1/n,n)
+  mu_pm = logit(x/nb)
+  mu_pm[mu_pm==-Inf] = logit(0.1)
+  mu_pm[mu_pm==Inf] = logit(0.9)
+  mu_pv = rep(1/n,n)
+  if(is.null(sigma2_init)){
+    sigma2 = var(mu_pm-b_pm)
+  }else{
+    sigma2 = sigma2_init
+  }
+  t_start = Sys.time()
+  for (iter in 1:maxiter) {
+
+    opt = vga_binomial(c(mu_pm,log(mu_pv)),x,nb,b_pm,sigma2,gh_points=gh_points)
+    mu_pm = opt$m
+    mu_pv = opt$v
+    # EBNM
+    res = ebnm(mu_pm,sqrt(sigma2),
+               mode=ebnm_params$mode,
+               prior_family=ebnm_params$prior_family,
+               scale = ebnm_params$scale,
+               g_init = ebnm_params$g_init,
+               fix_g = ebnm_params$fix_g,
+               output = ebnm_params$output,
+               optmethod = ebnm_params$optmethod)
+    b_pm = res$posterior$mean
+    b_pv = res$posterior$sd^2
+    H = res$log_likelihood + n*(log(2*pi*sigma2)/2)+sum((mu_pm^2-2*mu_pm*b_pm+b_pm^2+b_pv)/sigma2/2)
+
+    # Update sigma2
+    sigma2 = mean(mu_pm^2+mu_pv+b_pm^2+b_pv-2*b_pm*mu_pm)
+
+    # ELBO
+    obj[iter+1] = sum(x*mu_pm-nb*Elog1pexp(mu_pm,mu_pv,gh_points)) +const - n/2*log(2*pi*sigma2)
+    - sum(mu_pm^2 + mu_pv + b_pm^2 + b_pv - 2*mu_pm*b_pm)/2/sigma2 + H + sum(log(2*pi*mu_pv))/2 - n/2
+
+    if(iter%%printevery==0){
+      print(paste('At iter', iter, 'elbo=',round(obj[iter+1],3),'sigma2=',round(sigma2,3)))
+    }
+    if(convergence_criteria=='obj'){
+      if((obj[iter+1]-obj[iter])<tol){
+        obj = obj[1:(iter+1)]
+        if((obj[iter+1]-obj[iter])<0){
+          warning('An iteration decreases ELBO. This is likely due to numerical issues.')
+        }
+        break
+      }
+    }
+    if(convergence_criteria=='objabs'){
+      if(abs(obj[iter+1]-obj[iter])<tol){
+        obj = obj[1:(iter+1)]
+        break
+      }
+    }
+
+
+  }
+  t_end = Sys.time()
+  return(list(posterior = list(mean_logit = mu_pm,
+                               mean_b = b_pm,
+                               mean = Esigmoid(mu_pm,mu_pv,gh_points)),
+              fitted_g = list(sigma2=sigma2,g_b = res$fitted_g),
+              elbo=obj[length(obj)],
+              obj_trace = obj,
+              fit = res,
+              run_time = difftime(t_end,t_start,units='secs')))
+
+}
+
+
+
+
+vga_binomial = function(init_val,x,nb,beta,sigma2,method='lbfgs',gh_points){
+  if(method!="lbfgs"){
+    stop('only lbfgs is implemented')
+  }
+  n = length(x)
+  opt = try(optim(init_val,
+                  fn = vga_binom_obj,
+                  gr = vga_binom_obj_grad,
+                  x=x,
+                  nb=nb,
+                  beta=beta,
+                  sigma2=sigma2,
+                  n=n,
+                  gh_points=gh_points,
+                  method = 'L-BFGS-B'),silent = TRUE)
+
+  if(class(opt)=='try-error'){
+    warning(paste(opt[1],'; returning the initialization values'))
+    return(list(m=init_val[1:n],v=exp(init_val[(n+1):(2*n)])))
+  }else{
+    return(list(m=opt$par[1:n],v=exp(opt$par[(n+1):(2*n)]),opt=opt))
+  }
+
+
+}
+
+
+vga_binom_obj = function(theta,x,nb,beta,sigma2,n,gh_points){
+  m = theta[1:n]
+  lv = theta[(n+1):(2*n)]
+  val = - sum(x*m - nb*Elog1pexp(m,exp(lv),gh_points) - (m^2+exp(lv)-2*m*beta)/2/sigma2 + lv/2)
+  return(val)
+}
+
+vga_binom_obj_grad = function(theta,x,nb,beta,sigma2,n,gh_points){
+  m = theta[1:n]
+  lv = theta[(n+1):(2*n)]
+  dm = - (x - nb*Elog1pexp_dm(m,exp(lv),gh_points) - (m-m*beta)/sigma2)
+  dlv = - (- nb*Elog1pexp_dlv(m,lv,gh_points) - exp(lv)/2/sigma2 + 1/2)
+  return(c(dm,dlv))
+}
+
+Elog1pexp = function(m,v,gh_points){
+  mat = outer(sqrt(2*v),gh_points$x,FUN='*') + m
+  return(c(vebpm:::log1pexp(mat)%*%gh_points$w)/sqrt(pi))
+}
+
+Elog1pexp_dm = function(m,v,gh_points){
+  mat = outer(sqrt(2*v),gh_points$x,FUN='*') + m
+  return(c(sigmoid(mat)%*%gh_points$w)/sqrt(pi))
+}
+
+Elog1pexp_dlv = function(m,lv,gh_points){
+  temp = outer(sqrt(2*exp(lv)),gh_points$x,FUN='*')
+  mat = temp + m
+  mat2 = temp/2
+  return(c((mat2*sigmoid(mat))%*%gh_points$w)/sqrt(pi))
+}
+
+# Elog1pexp = function(m,v,gh_points){
+#   res=c()
+#   for(i in 1:length(m)){
+#     res[i] = ghQuad(log1pexp_func,gh_points,m=m[i],v=v[i])/sqrt(pi)
+#   }
+#   return(res)
+# }
+#
+# log1pexp_func = function(x,m,v){
+#   return(vebpm:::log1pexp(sqrt(2*v)*x+m))
+# }
+
+logit = function(x){
+  log(x/(1-x))
+}
+
+sigmoid = function(x){
+  1/(1+exp(-x))
+}
+
+Esigmoid = function(m,v,gh_points){
+  mat = outer(sqrt(2*v),gh_points$x,FUN='*') + m
+  return(c(sigmoid(mat)%*%gh_points$w)/sqrt(pi))
+}
+
+# Esigmoid = function(m,v,gh_points){
+#   res=c()
+#   for(i in 1:length(m)){
+#     res[i] = ghQuad(sigmoid_func,gh_points,m=m[i],v=v[i])/sqrt(pi)
+#   }
+#   return(res)
+# }
+#
+# sigmoid_func = function(x,m,v){
+#   return(sigmoid(sqrt(2*v)*x+m))
+# }
+
+
+binomial_mean_GG = function(x,nb,beta=NULL,sigma2=NULL,maxiter=100,tol=1e-5,n_gh = 10,printevery = 1){
+
+  n = length(x)
+  m = logit(x/nb)
+  m[m==-Inf] = logit(0.1)
+  m[m==Inf] = logit(0.9)
+  v = rep(1/n,n)
+  gh_points = gaussHermiteData(n_gh)
+  const = sum(lfactorial(nb)-lfactorial(x)-lfactorial(nb-x))
+  obj = -Inf
+
+  for(iter in 1:maxiter){
+    beta = mean(m)
+    sigma2 = mean(m^2+v-2*m*beta+beta^2)
+
+    opt = vga_binomial(c(m,log(v)),x,nb,beta,sigma2,gh_points=gh_points)
+    m = opt$m
+    v = opt$v
+
+    # ELBO
+    obj[iter+1] = sum(x*m-nb*Elog1pexp(m,v,gh_points)) +const - n/2*log(2*pi*sigma2)
+    - sum(m^2 + v + beta^2 - 2*m*beta)/2/sigma2 + sum(log(2*pi*v))/2 - n/2
+
+    if(iter%%printevery==0){
+      print(paste('At iter', iter, 'elbo=',round(obj[iter+1],3)))
+    }
+    if((obj[iter+1]-obj[iter])<tol){
+      obj = obj[1:(iter+1)]
+      if((obj[iter+1]-obj[iter])<0){
+        warning('An iteration decreases ELBO. This is likely due to numerical issues.')
+      }
+      break
+    }
+  }
+
+  return(list(posterior = list(mean_logit = m,
+                               mean = Esigmoid(m,v,gh_points)),
+              fitted_g = list(beta=beta,sigma2=sigma2),
+              elbo=obj[length(obj)],
+              obj_trace = obj
+              ))
+
+}
+
+
+
+
+
---
+++ code/poisson_smooth/simulation_summary_smooth.R
@@ -0,0 +1,101 @@
+get_mse_mean = function(out,rm_method=NULL){
+  res = c()
+  for(i in 1:length(out$output)){
+    res = rbind(res,out$output[[i]]$mse_smooth)
+  }
+  methods = colnames(res)
+  if(!is.null(rm_method)){
+    idx = match(rm_method,methods)
+    res = res[,-idx]
+    methods = methods[-idx]
+  }
+  n_methods = length(methods)
+
+  res_mean = colMeans(res,na.rm = T)
+  res_sd = apply(res,2,sd,na.rm=T)
+
+  order_idx = order(res_mean,decreasing = F)
+  print(knitr::kable(cbind(res_mean[order_idx],res_sd[order_idx]),col.names = c('mean','sd')))
+
+  #mse_mle = apply((out$sim_data$X-out$sim_data$Mean)^2,1,mean)
+  #mse_relative = mse_all/mse_mle
+  datax = reshape2::melt(res,varnames =c('simu','methods'),value.name = 'mse')
+  print(datax %>%
+          ggplot(aes(x=methods, y=mse)) +
+          geom_boxplot()+
+          coord_flip())
+
+}
+
+
+get_runtime = function(out,rm_method=NULL){
+  res = c()
+  for(i in 1:length(out$output)){
+    res = rbind(res,out$output[[i]]$mse_smooth)
+  }
+
+  res_time = c()
+  for(i in 1:length(out$output)){
+    res_time = rbind(res_time,out$output[[i]]$run_times)
+  }
+
+  methods = colnames(res)
+  if(!is.null(rm_method)){
+    idx = match(rm_method,methods)
+    res = res[,-idx]
+    res_time = res_time[,-idx]
+    methods = methods[-idx]
+  }
+  n_methods = length(methods)
+
+  res_mean = colMeans(res,na.rm=T)
+  res_time_mean = colMeans(res_time,na.rm=T)
+
+  plot(res_time_mean,res_mean,xlab='run time',ylab='mse',pch = 1:n_methods,col=1:n_methods, main='mean estimation')
+  legend('topright',methods,pch=1:n_methods,col=1:n_methods)
+  #abline(h=1,lty=2,col='grey80')
+
+}
+
+
+
+plot_all_curves = function(out,method='smash_two_step_hetero'){
+  #x_min = apply(out$sim_data$X,2,min)
+  #x_max = apply(out$sim_data$X,2,max)
+  x_median = apply(out$sim_data$X,2,median)
+  plot(x_median,col='grey80',xlab='',ylab='x(median of all runs)',main=method)
+  #lines(x_min,type='p',col='grey80')
+  lines(exp(out$sim_data$b))
+  for(i in 1:length(out$output)){
+    if(class(out$output[[i]]$fitted_model[[method]])!='try-error'){
+      lines(out$output[[i]]$fitted_model[[method]]$posterior$mean_smooth,col='grey80')
+    }
+
+  }
+
+
+  res = c()
+  for(i in 1:length(out$output)){
+    res = rbind(res,out$output[[i]]$mse_smooth)
+  }
+  methods = colnames(res)
+  res0 = res[,match(method,methods)]
+  lines(out$output[[which.min(res0)]]$fitted_model[[method]]$posterior$mean_smooth,col=2,lwd=1.5)
+  lines(out$output[[which.max(res0)]]$fitted_model[[method]]$posterior$mean_smooth,col=4,lwd=1.5)
+
+  legend('topright',c('True mean','best fit','worst fit','all others'),lty=c(1,1,1,1),col=c(1,2,4,'grey80'))
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
