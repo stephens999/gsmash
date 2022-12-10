03e8922bb8b6e30060e80bad2f5ac9bbf5a7492e
DongyueXie <dongyxie@gmail.com>
2022-12-10 10:29:44 -0600

    revise

--- code/poisson_STM/real_PMF.R
+++ code/poisson_STM/real_PMF.R
@@ -4,8 +4,8 @@ library(stm)
 data(pbmc_facs)
 counts <- pbmc_facs$counts
 table(pbmc_facs$samples$subpop)
-## use only B cell and NK cell
-cells = pbmc_facs$samples$subpop%in%c('B cell', 'NK cell')
+## use certain cell types
+cells = pbmc_facs$samples$subpop%in%c('B cell', 'NK cell','CD34+')
 Y = counts[cells,]
 dim(Y)
 # filter out genes that has few expressions(3% cells)
@@ -16,22 +16,40 @@ sum(rowSums(Y)==0)
 sum(colSums(Y)==0)
 dim(Y)
 
-rm(counts)
-rm(genes)
-rm(cells)
 
 S = tcrossprod(c(rowSums(Y)),c(colSums(Y)))/sum(Y)
 Y = as.matrix(Y)
 # run method
-fit = splitting_PMF_flashier(Y,S,var_type = 'by_col',Kmax = 30,maxiter = 5000,verbose = TRUE,n_cores = 10)
-saveRDS(fit,file='output/poisson_MF_simulation/fit_pbmc_2cells.rds')
+fit = splitting_PMF_flashier(Y,S,var_type = 'by_col',Kmax = 30,maxiter = 5000,verbose = TRUE,n_cores = 10,maxiter_backfitting = 1)
+saveRDS(fit,file='output/poisson_MF_simulation/fit_pbmc_3cells.rds')
 
 # transform Y and fit flashier
 a = c(apply(S,2,median))
 Y_normed = log((Y/S)*(a/0.5)+1)
 Y_normed = Matrix(Y_normed,sparse = T)
-fit_flashier = flashier::flash(Y_normed,var.type = 2,greedy.Kmax = 10,backfit = T)
-saveRDS(fit_flashier,file='output/poisson_MF_simulation/fit_flashier_pbmc_2cells.rds')
+fit_flashier = flashier::flash(Y_normed,var.type = 2,greedy.Kmax = 30,backfit = T,nullcheck = TRUE)
+saveRDS(fit_flashier,file='output/poisson_MF_simulation/fit_flashier_pbmc_3cells.rds')
 
 fit_svd = svd(Y_normed)
-saveRDS(fit_svd,file='output/poisson_MF_simulation/fit_svd_pbmc_2cells.rds')
+saveRDS(fit_svd,file='output/poisson_MF_simulation/fit_svd_pbmc_3cells.rds')
+
+########################
+counts <- pbmc_facs$counts
+S = tcrossprod(c(rowSums(counts)),c(colSums(counts)))/sum(counts)
+a = c(apply(S,2,median))
+Y_normed = log(t(t((counts/S))*(a/0.5))+1)
+rm(S)
+Y_normed = Matrix(Y_normed,sparse = T)
+fit_flashier = flashier::flash(Y_normed,var.type = 2,greedy.Kmax = 30,backfit = T,nullcheck = TRUE)
+saveRDS(fit_flashier,file='output/poisson_MF_simulation/fit_flashier_pbmc.rds')
+########################
+counts <- pbmc_facs$counts
+genes = (colSums(counts>0) > 10)
+counts = counts[,genes]
+S = tcrossprod(c(rowSums(counts)),c(colSums(counts)))/sum(counts)
+a = c(apply(S,2,median))
+Y_normed = log(t(t((counts/S))*(a/0.5))+1)
+rm(S)
+Y_normed = Matrix(Y_normed,sparse = T)
+fit_flashier = flashier::flash(Y_normed,var.type = 2,greedy.Kmax = 30,backfit = T,nullcheck = TRUE)
+saveRDS(fit_flashier,file='output/poisson_MF_simulation/fit_flashier_pbmc_filter_gene.rds')
